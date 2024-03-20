920fc55402d98f996471c31c61043642080a1c30
noeppi_noeppi <noeppinoeppi@gmail.com>
2024-03-20 22:57:30 +0100

    test

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 docker build -t 'images/actions' images/actions
 docker build -t 'images/java17' --build-arg="JDK=jdk-17.0.10%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.10_7.tar.gz" images/java
 docker build -t 'images/java21' --build-arg="JDK=jdk-21.0.2%2B13/OpenJDK21U-jdk_x64_linux_hotspot_21.0.2_13.tar.gz" images/java
-docker build -t 'images/texlive' images/texlive
+#docker build -t 'images/texlive' images/texlive
--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ set -e
 docker push 'images/actions:latest'
 docker push 'images/java17:latest'
 docker push 'images/java21:latest'
-docker push 'images/texlive:latest'
+#docker push 'images/texlive:latest'
