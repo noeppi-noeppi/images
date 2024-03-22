da64f041805d905cd2cd25bc3ace2bba73626c66
noeppi_noeppi <noeppinoeppi@gmail.com>
2024-03-22 13:36:08 +0100

    Use date as tag

--- publish.sh
+++ publish.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 function publish {
+  local DATE="$(date +'%Y%m%d')"
+  local DISCRIMINATOR=0
+  local TAG_NAME="${DATE}"
+  while docker manifest inspect "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}" > /dev/null 2> /dev/null; do
+    DISCRIMINATOR=$((DISCRIMINATOR+1))
+    TAG_NAME="${DATE}-${DISCRIMINATOR}"
+  done
+  docker tag "localhost/images/${1}:latest" "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}"
+  docker push "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}"
   docker tag "localhost/images/${1}:latest" "ghcr.io/noeppi-noeppi/images/${1}:latest"
   docker push "ghcr.io/noeppi-noeppi/images/${1}:latest"
-  local COMMIT_COUNT="$(git rev-list HEAD --count)"
-  if [[ "$(git rev-parse --symbolic-full-name HEAD)" == "refs/heads/master" && ! -z "${COMMIT_COUNT}" ]]; then
-    docker tag "localhost/images/${1}:latest" "ghcr.io/noeppi-noeppi/images/${1}:${COMMIT_COUNT}"
-    docker push "ghcr.io/noeppi-noeppi/images/${1}:${COMMIT_COUNT}"
-  fi
 }
 
 set -e
