83319e1bab3267708617d0dcd3ae598055203142
noeppi_noeppi <noeppinoeppi@gmail.com>
2024-03-22 12:00:45 +0100

    Also publish with the current commit as tag.

--- publish.sh
+++ publish.sh
@@ -3,6 +3,11 @@
 function publish {
   docker tag "localhost/images/${1}:latest" "ghcr.io/noeppi-noeppi/images/${1}:latest"
   docker push "ghcr.io/noeppi-noeppi/images/${1}:latest"
+  local COMMIT="$(git rev-parse --short HEAD)"
+  if [[ ! -z "${COMMIT}" ]]; then
+    docker tag "localhost/images/${1}:latest" "ghcr.io(noeppi-noeppi/images/${1}:${COMMIT}"
+    docker push "ghcr.io/noeppi-noeppi/images/${1}:${COMMIT}"
+  fi
 }
 
 set -e
