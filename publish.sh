015fe2eea3db6baff56155cef136ef49b5053623
noeppi_noeppi <noeppinoeppi@gmail.com>
2024-03-22 12:42:39 +0100

    Publish with a tag that contains the number of git commits.

--- publish.sh
+++ publish.sh
@@ -3,10 +3,10 @@
 function publish {
   docker tag "localhost/images/${1}:latest" "ghcr.io/noeppi-noeppi/images/${1}:latest"
   docker push "ghcr.io/noeppi-noeppi/images/${1}:latest"
-  local COMMIT="$(git rev-parse --short HEAD)"
-  if [[ ! -z "${COMMIT}" ]]; then
-    docker tag "localhost/images/${1}:latest" "ghcr.io(noeppi-noeppi/images/${1}:${COMMIT}"
-    docker push "ghcr.io/noeppi-noeppi/images/${1}:${COMMIT}"
+  local COMMIT_COUNT="$(git rev-list HEAD --count)"
+  if [[ "$(git rev-parse --symbolic-full-name HEAD)" == "refs/heads/master" && ! -z "${COMMIT_COUNT}" ]]; then
+    docker tag "localhost/images/${1}:latest" "ghcr.io/noeppi-noeppi/images/${1}:${COMMIT_COUNT}"
+    docker push "ghcr.io/noeppi-noeppi/images/${1}:${COMMIT_COUNT}"
   fi
 }
 
