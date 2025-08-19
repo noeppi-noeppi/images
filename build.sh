72d9478df08709ec6c493a261d1c47904ead00d6
noeppi_noeppi <noeppinoeppi@gmail.com>
2025-08-19 18:45:04 +0200

    fix publish script

--- build.sh
+++ build.sh
@@ -21,13 +21,13 @@ function build {
   if arrayContains "publish" "${ACTIONS[@]}"; then
     local DISCRIMINATOR=0
     local TAG_NAME="${DATE}"
-    while docker manifest inspect "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}" > /dev/null 2> /dev/null; do
+    while docker manifest inspect "ghcr.io/noeppi-noeppi/images/${IMG}:${TAG_NAME}" > /dev/null 2> /dev/null; do
       DISCRIMINATOR=$((DISCRIMINATOR+1))
       TAG_NAME="${DATE}-${DISCRIMINATOR}"
     done
-    docker tag "ghcr.io/noeppi-noeppi/images/${1}:latest" "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}"
-    docker push "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}"
-    docker push "ghcr.io/noeppi-noeppi/images/${1}:latest"
+    docker tag "ghcr.io/noeppi-noeppi/images/${IMG}:latest" "ghcr.io/noeppi-noeppi/images/${IMG}:${TAG_NAME}"
+    docker push "ghcr.io/noeppi-noeppi/images/${IMG}:${TAG_NAME}"
+    docker push "ghcr.io/noeppi-noeppi/images/${IMG}:latest"
   fi
 }
 
