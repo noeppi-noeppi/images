96f632cd3ec6e766e354d6b31af3fcb450b95f45
noeppi_noeppi <noeppinoeppi@gmail.com>
2025-08-19 18:33:51 +0200

    update to debian trixie and node24

--- build.sh
+++ build.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
+set -e -o pipefail
+
+DATE="$(date +'%Y%m%d')"
+ACTIONS=("$@")
+
+function arrayContains {
+  local ELEM MATCH="${1}"
+  shift
+  for ELEM; do [[ "${ELEM}" == "${MATCH}" ]] && return 0; done
+  return 1
+}
+
 function build {
   local IMG="${1}"
   shift
-  docker build -t "localhost/images/${IMG}:latest" "$@"
+  if arrayContains "build" "${ACTIONS[@]}"; then
+    docker build -t "ghcr.io/noeppi-noeppi/images/${IMG}:latest" "$@"
+  fi
+  if arrayContains "publish" "${ACTIONS[@]}"; then
+    local DISCRIMINATOR=0
+    local TAG_NAME="${DATE}"
+    while docker manifest inspect "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}" > /dev/null 2> /dev/null; do
+      DISCRIMINATOR=$((DISCRIMINATOR+1))
+      TAG_NAME="${DATE}-${DISCRIMINATOR}"
+    done
+    docker tag "ghcr.io/noeppi-noeppi/images/${1}:latest" "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}"
+    docker push "ghcr.io/noeppi-noeppi/images/${1}:${TAG_NAME}"
+    docker push "ghcr.io/noeppi-noeppi/images/${1}:latest"
+  fi
 }
 
-set -e
+if [[ "${#ACTIONS[@]}" -eq 0 ]]; then
+  echo "No action."
+  echo "Usage: ${0} <build|publish>..."
+  exit 1
+fi
+for ACTION in "${ACTIONS[@]}"; do
+  if [[ "${ACTION}" != "build" && "${ACTION}" != "publish" ]]; then
+    echo "Usage: ${0} <build|publish>..."
+    exit 1
+  fi
+done
 
 build 'actions' images/actions
 build 'java17' --build-arg="JDK=17" images/java
